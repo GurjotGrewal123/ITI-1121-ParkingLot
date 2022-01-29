9dc5e32a1c158169bbaed6d581e1414623cfd82c
Gurjot Grewal <gurjot_grewal@outlook.com>
2022-01-29 17:41:35 -0500

    First init

---
+++ a1_3000000_3000001/Car.java
@@ -0,0 +1,68 @@
+/**
+ * @author Mehrdad Sabetzadeh, University of Ottawa
+ */
+public class Car {
+
+	/**
+	 * Instance variable for storing the type of the car
+	 */
+	private CarType type;
+
+	/**
+	 * Instance variable for storing the car plate number
+	 */
+	private String plateNum;
+
+	/**
+	 * @return the type of this car
+	 */
+	public CarType getType() {
+	
+		return this.type; 
+	}
+
+	/**
+	 * Sets the type of the car
+	 * 
+	 * @param type is the car type
+	 */
+	public void setType(CarType type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the plate number
+	 */
+	public String getPlateNum() {
+		return this.plateNum; 
+	}
+
+	/**
+	 * Sets the car plate number
+	 * 
+	 * @param plateNum is the car plate number
+	 */
+	public void setPlateNum(String plateNum) {
+		this.plateNum = plateNum;
+	}
+
+	/**
+	 * Constructor for Car
+	 * 
+	 * @param type     is the type of the car
+	 * @param plateNum is the car plate number
+	 */
+	public Car(CarType type, String plateNum) {
+		this.plateNum = plateNum;
+		this.type = type;
+	}
+
+	/**
+	 * Returns a string representation of the car
+	 */
+	public String toString() {
+		// NOTE: The implementation of this method is complete. You do NOT need to
+		// change it for the assignment.
+		return Util.getLabelByCarType(type) + '(' + plateNum + ')';
+	}
+}
